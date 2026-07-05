024ece7e8990a3d2b555cd35aa08465721c0f2ba
Gaddiel Wambo-Becker <gaddielwb@gmail.com>
2026-07-05 19:58:17 +0200

    Added MLP

---
+++ src/models/bigram.h
@@ -0,0 +1,37 @@
+#ifndef BIGRAM_H
+#define BIGRAM_H
+
+#include <torch/torch.h>
+
+class Bigram {
+public:
+  torch::Tensor w1;
+
+  Bigram() {
+    torch::Generator g =
+        torch::make_generator<at::CPUGeneratorImpl>(2147483647);
+    w1 = torch::randn({27, 27}, g, torch::requires_grad(true));
+  }
+
+  torch::Tensor forward(torch::Tensor X) {
+    torch::Tensor xenc = torch::one_hot(X, 27).to(torch::kFloat32);
+
+    torch::Tensor Z = torch::matmul(xenc, w1);
+
+    return Z;
+  }
+
+  torch::Tensor loss(torch::Tensor logits, torch::Tensor Y) {
+    return torch::nn::functional::cross_entropy(logits, Y);
+  }
+
+  torch::Tensor backward(torch::Tensor loss) { loss.backward(); }
+
+  torch::Tensor grad_des(double lr) {
+    torch::NoGradGuard no_grad;
+
+    w1 = w1 - (w1.grad() * lr);
+  }
+};
+
+#endif
---
+++ src/models/mlp.h
@@ -0,0 +1,54 @@
+#ifndef MLP_H
+#define MLP_H
+
+#include <torch/torch.h>
+
+class MLP {
+public:
+  torch::Tensor C;
+  torch::Tensor w1;
+  torch::Tensor b1;
+  torch::Tensor w2;
+  torch::Tensor b2;
+  int context_len;
+
+  MLP(int context_len) {
+    torch::Generator g =
+        torch::make_generator<at::CPUGeneratorImpl>(2147483647);
+    torch::TensorOptions options = torch::requires_grad(true).device(
+        torch::kXPU); // .available() ? torch::kXPU, torch::kCPU);
+
+    int emb_dim = 10;
+    this->context_len = context_len;
+    C = torch::randn({27, emb_dim}, g, torch::requires_grad(true));
+    w1 = torch::randn({context_len * emb_dim, 300}, g);
+    b1 = torch::randn({300}, g);
+    w2 = torch::randn({300, 27}, g);
+    b2 = torch::randn({27}, g);
+  }
+
+  torch::Tensor forward(torch::Tensor X) {
+    torch::Tensor xemb =
+        torch::matmul(torch::one_hot(X, 27).to(torch::kFloat32), C);
+    xemb = xemb.view({-1, (context_len * C.size(1))});
+
+    torch::Tensor Z1 = torch::tanh(torch::matmul(xemb, w1) + b1);
+    torch::Tensor Z2 = torch::matmul(Z1, w2) + b2;
+
+    return Z2;
+  }
+
+  torch::Tensor loss(torch::Tensor logits, torch::Tensor Y) {
+    return torch::nn::functional::cross_entropy(logits, Y);
+  }
+
+  torch::Tensor backward(torch::Tensor loss) { loss.backward(); }
+
+  torch::Tensor grad_des(double lr) {
+    torch::NoGradGuard no_grad;
+
+    w1 = w1 - (w1.grad() * lr);
+  }
+};
+
+#endif
